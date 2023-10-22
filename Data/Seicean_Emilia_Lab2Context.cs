21d3c2bb5468aba1eb1dbfcd0cc84e20ade6da73
Asus <Asus@EMI>
2023-10-22 22:50:00 +0300

    Add project files.

---
+++ Data/Seicean_Emilia_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Seicean_Emilia_Lab2.Models;
+
+namespace Seicean_Emilia_Lab2.Data
+{
+    public class Seicean_Emilia_Lab2Context : DbContext
+    {
+        public Seicean_Emilia_Lab2Context (DbContextOptions<Seicean_Emilia_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Seicean_Emilia_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Seicean_Emilia_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Pages/Books/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Seicean_Emilia_Lab2.Data;
+using Seicean_Emilia_Lab2.Models;
+
+namespace Seicean_Emilia_Lab2.Pages.Books
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Seicean_Emilia_Lab2.Data.Seicean_Emilia_Lab2Context _context;
+
+        public CreateModel(Seicean_Emilia_Lab2.Data.Seicean_Emilia_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Book Book { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Book == null || Book == null)
+            {
+                return Page();
+            }
+
+            _context.Book.Add(Book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
